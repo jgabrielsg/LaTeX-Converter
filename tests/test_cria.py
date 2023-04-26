0e56a6c06095d0f7caba1a7939f1cb7655de7859
JOÃO GABRIEL MACHADO <B51065@fgv.edu.br>
2023-04-26 09:05:37 -0300

    Primeiro commit

---
+++ tests/test_cria.py
@@ -0,0 +1,7 @@
+import unittest
+from relatorio.cria import Relatorio
+
+class TestCria(unittest.TestCase):
+    def test_cria_relatorio(self):
+        R = Relatorio("Meu relatório", "Eu mesmo")
+        self.assertEqual(R.titulo, "Meu relatório")
